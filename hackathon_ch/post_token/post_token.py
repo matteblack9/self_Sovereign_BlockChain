3fd4a7ec49737801a5835ee8d5dba429b944b438
YoungHo-Jo <zeroho.jo@gmail.com>
2018-08-31 17:27:38 +0900

    Add some json files and change all of post_token.py
    because of self identity

--- hackathon_ch/post_token/post_token.py
+++ hackathon_ch/post_token/post_token.py
@@ -1,56 +1,68 @@
-from IPython import external
 from iconservice import *
 
 
 class POST_Token(IconScoreBase):
 
-    __DESIGNATOR = 'designator'
-    __IS_DELIVERED = 'is_delivered'
+    __USER_ADDRESS = 'user_address'
+    __ISSUERS = 'issuers'
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
 
-        self.__designator = VarDB(self.__DESIGNATOR, db, value_type=str)
-        self.__is_delivered = VarDB(self.__IS_DELIVERED, db, value_type=bool)
+        self.__users_address = DictDB(self.__USER_ADDRESS, db, value_type=str, depth=1) # default = 1
+        self.__issuers = DictDB(self.__ISSUERS, db, value_type=str, depth=1) # default = 1
 
-    def on_install(self, designator, delivered) -> None:
+    def on_install(self) -> None:
         super().on_install()
-
-        # self.__designator.set(designator)
-
-        self.__designator = designator
-
-        self.__is_delivered = delivered
 
     def on_update(self) -> None:
         super().on_update()
     
     @external(readonly=True)
-    def hello(self) -> str:
-        print(f'Hello, world!')
-        return "Hello"
-
-    @external(readonly=True)
-    def get_delivered_state(self) -> str:
-
-        if self.__is_delivered:
-            return "The mail was delivered"
-        else:
-            return "The mail is not delivered"
+    def get_my_claims(self) -> str:
+        claims = self.__get_array_of(str(self.msg.sender))
+        for e in claims:
+            print(e)
+        return "get_my_claims"
 
     @external
-    def delivered(self)->str:
-        if self.__is_delivered:
-            print(f'The mail has already delivered')
-        # elif str(self.msg.sender) == str(self.__designator.get()):
-        elif str(self.msg.sender) == str(self.__designator):
-            print(f'Change state of the mail')
-            self.__is_delivered = True
+    def add_claim(self, claim: str):
+        print(claim)
+        print(self.__get_array_of(str(self.msg.sender)).put(claim))
+
+    def __get_array_of(self, address: str):
+        return ArrayDB(address, self.db, value_type=str)
+
+    @external
+    def add_new_user(self, public_key: str):
+        if self.__users_address[str(self.msg.sender)] == 0: # not existed
+            self.__users_address[str(self.msg.sender)] = public_key
+            return "OK"
         else:
-            print(f'Error ')
+            return "FAILURE"
 
+    @external
+    def add_new_issuers(self, public_key: str):
+        if self.__issuers[str(self.msg.sender)] == 0: # not existed
+            self.__issuers[str(self.msg.sender)] = public_key
+            return "OK"
+        else:
+            return "FAILURE"
 
+    @external(readonly=True)
+    def get_user_public_key(self, user_address):
+        key = self.__users_address[str(user_address)]
+        if key == 0:
+            return "FAILURE"
+        else:
+            return key
 
-
+    @external(readonly=True)
+    def get_issuer_public_key(self, issuer_address):
+        key = self.__issuers[str(issuer_address)]
+        if key == 0:
+            return "FAILURE"
+        else:
+            return key
 
 
